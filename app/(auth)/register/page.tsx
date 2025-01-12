fe52d7112a82c6877bf68d2178836fd8999cb065
johnkatua <johnkatua99@gmail.com>
2025-01-12 18:44:50 +0300

    add create account title

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,7 +4,19 @@ import React from 'react'
 const Register = () => {
   return (
     <main className='h-screen bg-slate-100 font-serif flex justify-between p-4 gap-4'>
-      <div className='bg-slate-900 w-[50%]'>Left</div>
+      <div className='w-[50%] bg-slate-300 h-[100%] flex flex-col'>
+        <div>Logo</div>
+        <div className='flex  flex-1 justify-center items-center'>
+          <div className='bg-red-50 w-[400px]'>
+            <div className='text-center mb-2'>
+              <h2 className='text-2xl mb-2 font-semibold'>Create your account</h2>
+              <p className='text-slate-500'>Let's get started with you managing your finances</p>
+            </div>
+            <hr />
+            <div className='mt-2'>God</div>
+          </div>
+        </div>
+      </div>
       <div className='bg-slate-100 w-[50%]'>
         <div className='h-full relative rounded-lg'>
           <div className='absolute z-10 flex items-end justify-center mb-4 p-4 pb-12 text-center text-white w-[100%] h-[100%]'>
