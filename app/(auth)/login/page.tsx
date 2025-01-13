2685b7c0219b40ee9fa86a2749e9cc462904e0fd
johnkatua <johnkatua99@gmail.com>
2025-01-13 11:08:02 +0300

    add a structure for left part of the login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,8 +3,14 @@ import React from "react";
 const Login = () => {
   return (
     <main className="h-screen font-serif flex justify-between p-4 gap-4">
-      <div>Left</div>
-      <div>Right</div>
+      <div className="bg-slate-300 w-[50%]">
+        <div className="h-full relative rounded-lg">
+          <div>Logo</div>
+          <div>Image</div>
+          <div>Brief Description</div>
+        </div>
+      </div>
+      <div className="bg-slate-300 w-[50%]">Right</div>
     </main>
   );
 };
