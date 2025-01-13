ee763cf80770eed254d31c4f9c0e25272fa82a64
johnkatua <johnkatua99@gmail.com>
2025-01-13 11:22:45 +0300

    add a title for the login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -10,7 +10,18 @@ const Login = () => {
           <div>Brief Description</div>
         </div>
       </div>
-      <div className="bg-slate-300 w-[50%]">Right</div>
+      <div className="w-[50%] flex justify-center items-center">
+        <div className="w-[560px]">
+          <div className="text-center mb-2">
+            <h2 className="text-2xl mb-2 font-semibold">Welcome back!</h2>
+            <p className="text-slate-500">
+              Zero One is the best platform to manage your finances. Start and
+              enjoy your journey 🤘
+            </p>
+          </div>
+        </div>
+        <hr />
+      </div>
     </main>
   );
 };
