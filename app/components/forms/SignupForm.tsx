7554aae8573ad13d84d68c48f073a073784a4ff1
johnkatua <johnkatua99@gmail.com>
2025-01-13 08:38:22 +0300

    add form fields for email and password fields

--- app/components/forms/SignupForm.tsx
+++ app/components/forms/SignupForm.tsx
@@ -28,6 +28,16 @@ const SignupForm = () => {
         <CustomFormField control={form.control} name="name" label="Name">
           <Input placeholder="Test User" />
         </CustomFormField>
+        <CustomFormField control={form.control} name="email" label="Email">
+          <Input placeholder="test.user@gmail.com" />
+        </CustomFormField>
+        <CustomFormField
+          control={form.control}
+          name="password"
+          label="Password"
+        >
+          <Input placeholder="***********" type="password" />
+        </CustomFormField>
       </form>
     </Form>
   );
