3dfee9d00e46951b2546fa0b1a05ab6b11c9a6eb
johnkatua <johnkatua99@gmail.com>
2025-01-13 13:36:29 +0300

    add types for the app store and root state

--- lib/store.ts
+++ lib/store.ts
@@ -6,3 +6,8 @@ export const makeStore = () => {
     reducer: {},
   });
 };
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
