d46c8529a74784199b92668ab0921a2e640a1ccb
johnkatua <johnkatua99@gmail.com>
2025-01-13 11:58:26 +0300

    add custom field for email and password inputs

--- app/components/forms/SigninForm.tsx
+++ app/components/forms/SigninForm.tsx
@@ -5,12 +5,31 @@ import { signinFormSchema } from "@/lib/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import CustomFormField from "./CustomFormField";
+import { Input } from "@/components/ui/input";
 
 const SigninForm = () => {
   const form = useForm<z.infer<typeof signinFormSchema>>({
     resolver: zodResolver(signinFormSchema),
   });
-  return <Form></Form>;
+
+  const { control } = form;
+  return (
+    <Form {...form}>
+      <form>
+        <CustomFormField control={control} name="email" label="Email">
+          <Input placeholder="test.user@gmail.com" />
+        </CustomFormField>
+        <CustomFormField
+          control={form.control}
+          name="password"
+          label="Password"
+        >
+          <Input placeholder="***********" type="password" />
+        </CustomFormField>
+      </form>
+    </Form>
+  );
 };
 
 export default SigninForm;
