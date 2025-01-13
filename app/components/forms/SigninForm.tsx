22b9a7aba35ab640d2debbca6034040373f6782b
johnkatua <johnkatua99@gmail.com>
2025-01-13 11:54:44 +0300

    add a useForm hook to handle user input

--- app/components/forms/SigninForm.tsx
+++ app/components/forms/SigninForm.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import { Form } from "@/components/ui/form";
+import { signinFormSchema } from "@/lib/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const SigninForm = () => {
+  const form = useForm<z.infer<typeof signinFormSchema>>({
+    resolver: zodResolver(signinFormSchema),
+  });
   return <Form></Form>;
 };
 
