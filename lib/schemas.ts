d144e063a349bc87af8a07efc5844b3ee70fea40
johnkatua <johnkatua99@gmail.com>
2025-01-13 11:47:40 +0300

    add signin form schema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -3,5 +3,10 @@ import { z } from "zod";
 export const signupFormSchema = z.object({
   name: z.string().min(2).max(50),
   email: z.string(),
-  password: z.string(),
+  password: z.string().min(6),
+});
+
+export const signinFormSchema = z.object({
+  email: z.string(),
+  password: z.string().min(6),
 });
