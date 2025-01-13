fba676f756c09c80afcd2837569ddf536da4ff6a
johnkatua <johnkatua99@gmail.com>
2025-01-13 18:27:57 +0300

    add theme colors for the app

--- app/(dashboard)/index.tsx
+++ app/(dashboard)/index.tsx
@@ -1,5 +1,10 @@
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="bg-black text-white h-screen">
+      <div>Left</div>
+      <div>Right</div>
+    </div>
+  );
 };
 
 export default Dashboard;
