cc50912cc916f6f2a89b0d66edb7d4db9df17d7b
johnkatua <johnkatua99@gmail.com>
2025-01-13 08:05:44 +0300

    remove unused form fields name update username to name

--- app/components/forms/SignupForm.tsx
+++ app/components/forms/SignupForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Form } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -8,38 +8,22 @@ import { z } from "zod"
 import CustomFormField from "./CustomFormField"
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50)
+  name: z.string().min(2).max(50)
 })
 
 const SignupForm = () => {
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: ""
+      name: ""
     }
   })
   return (
     <Form {...form}>
       <form className="px-2 py-2">
-        <CustomFormField control={form.control} name="username" label="Username">
+        <CustomFormField control={form.control} name="name" label="Username">
           <Input placeholder="Test User" />
         </CustomFormField>
-        {/* <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Test User" {...field} required />
-              </FormControl>
-              <FormDescription>
-                This is your public display name
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        /> */}
       </form>
     </Form>
   )
