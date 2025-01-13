619dc9f31163b30d47526d93d09edee574dbece2
johnkatua <johnkatua99@gmail.com>
2025-01-13 13:56:30 +0300

    return store provider to be used in the app

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -1,9 +1,20 @@
 "use client";
 
-import { ReactNode } from "react";
+import { AppStore, makeStore } from "@/lib/store";
+import { ReactNode, useRef } from "react";
+import { Provider } from "react-redux";
 
 interface StoreProviderProps {
   children: ReactNode;
 }
 
-export const StoreProvider = ({ children }: StoreProviderProps) => {};
+export const StoreProvider = ({ children }: StoreProviderProps) => {
+  const storeRef = useRef<AppStore | null>(null);
+
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore();
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
+};
