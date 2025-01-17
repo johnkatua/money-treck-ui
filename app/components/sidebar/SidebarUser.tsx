12d33c4336ef80410b239d1b3b6b6dcef936a889
johnkatua <johnkatua99@gmail.com>
2025-01-17 20:08:47 +0300

    add dropdown menu component

---
+++ app/components/sidebar/SidebarUser.tsx
@@ -0,0 +1,12 @@
+import { SidebarMenu, SidebarMenuItem } from "@/components/ui/sidebar";
+import React from "react";
+
+const SidebarUser = () => {
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>{/* <Dropdo */}</SidebarMenuItem>
+    </SidebarMenu>
+  );
+};
+
+export default SidebarUser;
