4f694e40aac618bf6390578a21efe324cf475e45
johnkatua <johnkatua99@gmail.com>
2025-01-17 18:07:02 +0300

    add sidebar and main body

--- app/(dashboard)/index.tsx
+++ app/(dashboard)/index.tsx
@@ -1,8 +1,12 @@
 const Dashboard = () => {
   return (
-    <div className="bg-[--theme-color-one] text-white h-screen">
-      <div>Left</div>
-      <div>Right</div>
+    <div className="bg-[--theme-color-one] text-white h-screen flex gap-4">
+      <div className="w-[300px] bg-slate-500 p-2">Left</div>
+      <div className="flex-1 py-4 pr-4">
+        <div className="h-[100%] p-2 bg-white text-[--theme-color-one] rounded-lg">
+          Right
+        </div>
+      </div>
     </div>
   );
 };
