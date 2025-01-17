01375e40319fb2f98aae225e5682dc2cdeaedcf9
johnkatua <johnkatua99@gmail.com>
2025-01-17 19:43:13 +0300

    add overflow to the parent sidebar

--- app/(dashboard)/index.tsx
+++ app/(dashboard)/index.tsx
@@ -1,7 +1,11 @@
+import CustomSidebar from "../components/sidebar/CustomSidebar";
+
 const Dashboard = () => {
   return (
     <div className="bg-[--theme-color-one] text-white h-screen flex gap-4">
-      <div className="w-[300px] p-2">Left</div>
+      <div className="w-[300px] p-2 overflow-hidden">
+        <CustomSidebar />
+      </div>
       <div className="flex-1 py-4 pr-4">
         <div className="h-[100%] p-2 bg-white text-[--theme-color-one] rounded-lg overflow-scroll">
           <div className="h-[900px]">Hello</div>
--- app/components/sidebar/CustomSidebar.tsx
+++ app/components/sidebar/CustomSidebar.tsx
@@ -3,18 +3,21 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarProvider,
   SidebarRail,
 } from "@/components/ui/sidebar";
 import React from "react";
 
 const CustomSidebar = () => {
   return (
-    <Sidebar>
-      <SidebarHeader>Header</SidebarHeader>
-      <SidebarContent>Hello</SidebarContent>
-      <SidebarFooter>Footer</SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
+    <SidebarProvider open={true}>
+      <Sidebar collapsible="icon" className="bg-green w-[300px]">
+        <SidebarHeader>Header</SidebarHeader>
+        <SidebarContent>Hello</SidebarContent>
+        <SidebarFooter>Footer</SidebarFooter>
+        {/* <SidebarRail /> */}
+      </Sidebar>
+    </SidebarProvider>
   );
 };
 
