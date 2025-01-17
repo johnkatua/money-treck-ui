ed0b089f69f337084c42ae2e42f41b19f66fc5af
johnkatua <johnkatua99@gmail.com>
2025-01-17 19:06:31 +0300

    add a sidebar component from shadcn

--- app/(dashboard)/index.tsx
+++ app/(dashboard)/index.tsx
@@ -1,7 +1,7 @@
 const Dashboard = () => {
   return (
     <div className="bg-[--theme-color-one] text-white h-screen flex gap-4">
-      <div className="w-[300px] bg-slate-500 p-2">Left</div>
+      <div className="w-[300px] p-2">Left</div>
       <div className="flex-1 py-4 pr-4">
         <div className="h-[100%] p-2 bg-white text-[--theme-color-one] rounded-lg overflow-scroll">
           <div className="h-[900px]">Hello</div>
