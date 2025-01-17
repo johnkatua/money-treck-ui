4cacf69afc2b8c6506edc1223e3aa05e777839bd
johnkatua <johnkatua99@gmail.com>
2025-01-17 18:30:17 +0300

    add sidebar data

---
+++ lib/data.ts
@@ -0,0 +1,36 @@
+export const sidebarData = {
+  user: {
+    name: "John Katua",
+    email: "user.me@gmail.com",
+    avatar: "/images/avatar.jpg",
+  },
+  items: [
+    {
+      title: "Dashboard",
+      url: "",
+      icon: "",
+    },
+    {
+      title: "Revenue",
+      url: "",
+      icon: "",
+    },
+    {
+      title: "Budget",
+      url: "",
+      icon: "",
+    },
+    {
+      title: "Expenditure",
+      url: "",
+      icon: "",
+    },
+    {
+      title: "Settings",
+      url: "",
+      icon: "",
+    },
+  ],
+};
+
+// <a href="https://www.freepik.com/free-vector/young-man-glasses-hoodie_356306296.htm">Image by brgfx on Freepik</a>
