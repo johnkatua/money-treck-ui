73db5ce84a3c2bafa30a237b3c28211eea43ac63
johnkatua <johnkatua99@gmail.com>
2025-01-17 19:10:15 +0300

    add sidebar header, content and footer

--- app/components/sidebar/CustomSidebar.tsx
+++ app/components/sidebar/CustomSidebar.tsx
@@ -1,7 +1,21 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
 import React from "react";
 
 const CustomSidebar = () => {
-  return <div></div>;
+  return (
+    <Sidebar>
+      <SidebarHeader>Header</SidebarHeader>
+      <SidebarContent>Hello</SidebarContent>
+      <SidebarFooter>Footer</SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
 };
 
 export default CustomSidebar;
