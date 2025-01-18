06ca58f4508e6ba2171b5fbcf63e9e2882d19325
johnkatua <johnkatua99@gmail.com>
2025-01-18 15:36:21 +0300

    update dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from "react";
 import CustomSidebar from "../components/sidebar/CustomSidebar";
 
-const Dashboard = () => {
+const Dashboard = ({ children }: { children: ReactNode }) => {
   return (
     <div className="bg-[--theme-color-one] text-white h-screen flex gap-4">
       <div className="w-[200px] p-2 overflow-hidden">
@@ -8,7 +9,7 @@ const Dashboard = () => {
       </div>
       <div className="flex-1 py-4 pr-4">
         <div className="h-[100%] p-2 bg-white text-[--theme-color-one] rounded-lg overflow-scroll">
-          <div className="h-[900px]">Hello</div>
+          <div className="h-[900px]">{children}</div>
         </div>
       </div>
     </div>
