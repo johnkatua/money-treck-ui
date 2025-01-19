721c446a667cc807346cefad61af3935b08ec4f2
johnkatua <johnkatua99@gmail.com>
2025-01-19 11:35:02 +0300

    update the revenue page

--- app/(dashboard)/revenue/page.tsx
+++ app/(dashboard)/revenue/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Dashboard from "../layout";
+import PageWrapper from "../../components/pageWrapper";
 
 const Revenue = () => {
-  return <div>Revenue</div>;
+  return (
+    <Dashboard>
+      <PageWrapper title="Revenue">
+        <div>Revenue</div>
+      </PageWrapper>
+    </Dashboard>
+  );
 };
 
 export default Revenue;
