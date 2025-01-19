8802d3a3758e6a47680d5b794b9288619e8549d9
johnkatua <johnkatua99@gmail.com>
2025-01-19 12:30:21 +0300

    update chart data

--- lib/data.ts
+++ lib/data.ts
@@ -47,4 +47,13 @@ export const sidebarData = {
   ],
 };
 
+export const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
 // <a href="https://www.freepik.com/free-vector/young-man-glasses-hoodie_356306296.htm">Image by brgfx on Freepik</a>
