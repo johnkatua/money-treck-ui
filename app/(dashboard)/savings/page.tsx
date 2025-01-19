325672a959f85daa91b25e1b6e61d1ca52391353
johnkatua <johnkatua99@gmail.com>
2025-01-19 12:04:59 +0300

    add a savings page

---
+++ app/(dashboard)/savings/page.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from "@/app/components/pageWrapper";
+
+const Savings = () => {
+  return (
+    <PageWrapper title="Savings">
+      <div>Savings</div>
+    </PageWrapper>
+  );
+};
+
+export default Savings;
