891b0764ba6870363c277ae089b1aceacb2b0635
johnkatua <johnkatua99@gmail.com>
2025-01-19 12:49:55 +0300

    add revenue chart to the dashboard

--- app/(dashboard)/index.tsx
+++ app/(dashboard)/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Dashboard from "./layout";
 import PageWrapper from "../components/pageWrapper";
+import RevenueBudgetChart from "../components/analytics/RevenueBudgetChart";
 
 const HomePage = () => {
   return (
     <Dashboard>
       <PageWrapper title="Dashboard">
-        <div>Welcome</div>
+        <div>
+          <RevenueBudgetChart />
+        </div>
       </PageWrapper>
     </Dashboard>
   );
