068b757f7804198b5212ea0e85b1b80a25d9d2b4
johnkatua <johnkatua99@gmail.com>
2025-01-20 19:24:31 +0300

    add a tabs content for the settings page

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 import PageWrapper from "@/app/components/page-wrapper";
+import GeneralSettings from "./GeneralSettings";
+import Plan from "./Plan";
+import Notifications from "./Notifications";
+
+const tabsContent = [
+  {
+    value: "General-Settings",
+    children: <GeneralSettings />,
+  },
+  {
+    value: "Notification",
+    children: <Notifications />,
+  },
+  {
+    value: "Plan",
+    children: <Plan />,
+  },
+];
 
 const Settings = () => {
   return (
