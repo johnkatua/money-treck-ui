5002160ae4a1266150e7463b4e0bb9bd3977455f
johnkatua <johnkatua99@gmail.com>
2025-01-20 19:21:53 +0300

    add multiple pages for the settings

---
+++ app/(dashboard)/settings/GeneralSettings.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GeneralSettings = () => {
+  return <div>General Settings</div>;
+};
+
+export default GeneralSettings;
---
+++ app/(dashboard)/settings/Plan.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Plan = () => {
+  return <div>Plans</div>;
+};
+
+export default Plan;
