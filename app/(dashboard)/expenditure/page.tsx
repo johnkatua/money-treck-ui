28e7c5a54c7f7a5d58a23e9135b36a452ca4f6b0
johnkatua <johnkatua99@gmail.com>
2025-01-20 10:04:37 +0300

    add tab wrapper to the expenses page

--- app/(dashboard)/expenditure/page.tsx
+++ app/(dashboard)/expenditure/page.tsx
@@ -1,9 +1,17 @@
 import PageWrapper from "@/app/components/pageWrapper";
+import TabWrapper from "@/app/components/tabWrapper";
+import { tabsHeader } from "@/lib/data";
+
+const tabsContent = [];
 
 const Expenses = () => {
   return (
     <PageWrapper title="Expenses">
-      <div>Expenses</div>
+      <TabWrapper
+        defaultValue="View"
+        tabsHeader={tabsHeader}
+        tabsContent={tabsContent}
+      />
     </PageWrapper>
   );
 };
