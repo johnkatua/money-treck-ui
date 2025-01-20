04a1083e659ecf5dcc3c527e0c3905b6e3baef73
johnkatua <johnkatua99@gmail.com>
2025-01-20 10:53:18 +0300

    define table config use react table hook

--- app/components/table-wrapper/index.tsx
+++ app/components/table-wrapper/index.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-
+import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
 const TableWrapper = () => {
+  const table = useReactTable({
+    data: [],
+    columns: [],
+    getCoreRowModel: getCoreRowModel(),
+    manualGrouping: true,
+    debugTable: true,
+  });
   return (
-    <div>
-      Table Wrapper
-      <div>Table</div>
+    <div className="p-2">
+      <div className="h-2" />
+      <table>
+        <thead></thead>
+        <tbody></tbody>
+      </table>
     </div>
   );
 };
