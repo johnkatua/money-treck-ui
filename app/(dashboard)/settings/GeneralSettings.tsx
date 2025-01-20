833cad38aca58da8a5a9b271efe2d17ea78a0614
johnkatua <johnkatua99@gmail.com>
2025-01-20 22:03:04 +0300

    update the general settings page

--- app/(dashboard)/settings/GeneralSettings.tsx
+++ app/(dashboard)/settings/GeneralSettings.tsx
@@ -1,7 +1,21 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 
 const GeneralSettings = () => {
-  return <div>General Settings</div>;
+  return (
+    <div className="grid grid-cols-1 gap-8">
+      <Card>
+        <CardHeader>
+          <CardTitle>Profile</CardTitle>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Themes</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 };
 
 export default GeneralSettings;
