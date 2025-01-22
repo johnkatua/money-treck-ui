073bf3b37c13dc937b13bd9aaf5d80513e5fd30e
johnkatua <johnkatua99@gmail.com>
2025-01-22 18:07:59 +0300

    add an action to login

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,7 +1,13 @@
-import { deleteSession } from "@/lib/session";
+import { createSession, deleteSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 
 export const logout = async () => {
   deleteSession();
   redirect("/login");
+};
+
+export const login = async (values) => {
+  console.log(values);
+  await createSession();
+  redirect("/");
 };
