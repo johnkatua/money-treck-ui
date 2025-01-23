cff84b8e8a8370d78b5e7773c05239db09c6f687
johnkatua <johnkatua99@gmail.com>
2025-01-24 00:21:40 +0300

    update use revenues query

--- app/hooks/useRevenue.ts
+++ app/hooks/useRevenue.ts
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { REVENEUS_QUERY_KEY } from "../constants";
 import { getRevenues } from "../services";
 
-export const useRevenue = () => {
+export const useRevenues = () => {
   useQuery({
     queryKey: REVENEUS_QUERY_KEY,
     queryFn: getRevenues,
