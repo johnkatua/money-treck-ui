4031f469b089402693ae21196d82fcc57115f228
johnkatua <johnkatua99@gmail.com>
2025-01-24 00:07:07 +0300

    update the query key

--- app/hooks/useRevenue.ts
+++ app/hooks/useRevenue.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "react-query";
 import { axiosInterceptorInstance } from "../api/axios-interceptor-instance";
+import { REVENEUS_QUERY_KEY } from "../constants";
 
 export const useRevenue = () => {
   useQuery({
-    queryKey: ["revenues"],
+    queryKey: REVENEUS_QUERY_KEY,
     queryFn: async () => await axiosInterceptorInstance.get("/"),
   });
 };
