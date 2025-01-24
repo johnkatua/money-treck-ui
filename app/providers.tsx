33db98fa77eee819d3bbe6f3722fb2c28d6a5852
johnkatua <johnkatua99@gmail.com>
2025-01-24 09:59:25 +0300

    update providers file type to tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,10 +3,9 @@
 import {
   isServer,
   QueryClient,
-  QueryClientProvider
+  QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactNode } from "react";
-
 
 const makeQueryClient = () => {
   return new QueryClient({
@@ -23,21 +22,16 @@ let browserQueryClient: QueryClient | undefined = undefined;
 const getQueryClient = () => {
   if (isServer) {
     return makeQueryClient();
-  }
-  else {
+  } else {
     if (!browserQueryClient) browserQueryClient = makeQueryClient();
     return browserQueryClient;
   }
 };
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
-  const queryClient = getQueryClient()
+export const Providers = ({ children }: { children: ReactNode }) => {
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-}
+  );
+};
