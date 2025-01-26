7d50c68460877d3debfe47bce036ca06b71e39e2
johnkatua <johnkatua99@gmail.com>
2025-01-26 21:41:18 +0300

    add a budget form

--- app/(dashboard)/budget/CreateBudget.tsx
+++ app/(dashboard)/budget/CreateBudget.tsx
@@ -1,7 +1,36 @@
+import CustomButton from "@/app/components/buttons/CustomButton";
+import CustomFormField from "@/app/components/forms/CustomFormField";
+import FormWrapper from "@/app/components/forms/FormWrapper";
+import { Input } from "@/components/ui/input";
+import { Select } from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const CreateBudget = () => {
-  return <div>Add Budget</div>;
+  const form = useForm<>({
+    resolver: zodResolver(),
+  });
+
+  const onSubmit = async (values) => {
+    console.log(values);
+  };
+
+  const { control } = form;
+  return (
+    <FormWrapper formTitle="Add New Budget" form={form} onSubmit={onSubmit}>
+      <CustomFormField control={control} name="name" label="Name *">
+        <Input placeholder="New Budget" />
+      </CustomFormField>
+      <CustomFormField control={control} name="amount" label="Amount *">
+        <Input />
+      </CustomFormField>
+      <CustomFormField control={control} name="period" label="Period *">
+        <Select></Select>
+      </CustomFormField>
+      <CustomButton text="Add Budget" type="submit" disabled={false} />
+    </FormWrapper>
+  );
 };
 
 export default CreateBudget;
