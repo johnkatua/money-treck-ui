25bd83af132a57f27d6a83e6ccf75428c2ff12de
johnkatua <johnkatua99@gmail.com>
2025-01-26 12:06:25 +0300

    fix: imports in the useRevenue hook

--- app/hooks/useRevenue.ts
+++ app/hooks/useRevenue.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import { REVENEUS_QUERY_KEY } from "../constants";
 import { getRevenues } from "../services";
 
