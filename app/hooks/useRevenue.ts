a6112eb51090794810ba9eebc73a9e6e23e2f815
johnkatua <johnkatua99@gmail.com>
2025-01-28 01:49:16 +0300

    cleanup use revenue hook

--- app/hooks/useRevenue.ts
+++ app/hooks/useRevenue.ts
@@ -3,21 +3,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { REVENEUS_QUERY_KEY } from "../constants";
 import { getRevenues } from "../services";
-import { Revenue } from "../types";
 
 export const useRevenues = () => {
-  // console.log(initialState);
   return useQuery({
     queryKey: REVENEUS_QUERY_KEY,
     queryFn: getRevenues,
-    // initialData: initialState,
-    // initialData: [
-    //   {
-    //     name: "John",
-    //     amount: 89,
-    //     period: "Daily",
-    //     user_id: "8990320",
-    //   },
-    // ],
   });
 };
