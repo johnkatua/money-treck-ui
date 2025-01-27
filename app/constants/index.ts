ca77abf3d29a18f25597cfc88a2ead4c95b8141a
johnkatua <johnkatua99@gmail.com>
2025-01-28 02:18:37 +0300

    add use-budget hook

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,2 +1,2 @@
 export const REVENEUS_QUERY_KEY = ["revenues"];
-export const BUDGET_QUERY_KEY = ["budget"];
+export const BUDGETS_QUERY_KEY = ["budgets"];
---
+++ app/hooks/useBudget.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBudgets } from "../services/budget";
+import { BUDGETS_QUERY_KEY } from "../constants";
+
+export const useBudget = () => {
+  return useQuery({
+    queryKey: BUDGETS_QUERY_KEY,
+    queryFn: getBudgets,
+  });
+};
