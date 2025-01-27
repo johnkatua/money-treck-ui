07e6092a3d434abf68ee3480ba75aaf87965a467
johnkatua <johnkatua99@gmail.com>
2025-01-28 01:48:01 +0300

    cleanup revenue list component

--- app/(dashboard)/revenue/RevenueList.tsx
+++ app/(dashboard)/revenue/RevenueList.tsx
@@ -1,26 +1,11 @@
 "use client";
 
 import TableWrapper from "@/app/components/table-wrapper";
-import { REVENEUS_QUERY_KEY } from "@/app/constants";
 import { useRevenues } from "@/app/hooks";
-import { getRevenues } from "@/app/services";
-import { Revenue, revenueColumns } from "@/lib/table-columns";
-
-const revenueData: Revenue[] = [
-  { id: "1", name: "Product A", amount: 12345 },
-  { id: "2", name: "Product B", amount: 67890 },
-  { id: "3", name: "Product C", amount: 23456 },
-  { id: "4", name: "Product D", amount: 78901 },
-  { id: "5", name: "Product E", amount: 34567 },
-  { id: "6", name: "Product F", amount: 89012 },
-  { id: "7", name: "Product G", amount: 45678 },
-  { id: "8", name: "Product H", amount: 90123 },
-  { id: "9", name: "Product I", amount: 56789 },
-  { id: "10", name: "Product J", amount: 12345 },
-];
+import { revenueColumns } from "@/lib/table-columns";
 
 const RevenueList = () => {
-  const { data, isPending, isFetching, isFetched, isLoading, isError, error } =
+  const { data, isPending, isFetching, isLoading, isError, error } =
     useRevenues();
 
   if (isError && data && data?.length < 0)
