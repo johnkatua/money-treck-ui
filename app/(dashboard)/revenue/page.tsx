744f2d7d3d56950bab6c633f2d56bb207eb3d518
johnkatua <johnkatua99@gmail.com>
2025-01-29 22:28:44 +0300

    update revenue page

--- app/(dashboard)/revenue/page.tsx
+++ app/(dashboard)/revenue/page.tsx
@@ -4,11 +4,20 @@ import CreateRevenue from "./CreateRevenue";
 import TabWrapper from "@/app/components/tab-wrapper";
 import { tabsHeader } from "@/lib/data";
 import RevenueListWrapper from "./RevenueListWrapper";
+import TableListWrapper from "@/app/components/table-wrapper/TableListWrapper";
+import RevenueList from "./RevenueList";
+import { REVENEUS_QUERY_KEY } from "@/app/constants";
+import { getRevenues } from "@/app/services";
 
 const tabsContent = [
   {
     value: "View",
-    children: <RevenueListWrapper />,
+    children: (
+      <TableListWrapper queryKey={REVENEUS_QUERY_KEY} queryFn={getRevenues}>
+        <RevenueList />
+      </TableListWrapper>
+    ),
+    // children: <RevenueListWrapper />,
   },
   {
     value: "Create",
