12755c91d5268f4c7b17dd71b654c15f2078f4cf
johnkatua <johnkatua99@gmail.com>
2025-01-29 22:18:49 +0300

    add a list wrapper

---
+++ app/components/table-wrapper/TableListWrapper.tsx
@@ -0,0 +1,31 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { FC, ReactNode } from "react";
+
+interface ITableListWrapper {
+  queryKey: string[];
+  queryFn: () => void;
+  children: ReactNode;
+}
+
+const TableListWrapper: FC<ITableListWrapper> = async ({
+  queryKey,
+  queryFn,
+  children,
+}) => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey,
+    queryFn,
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {children}
+    </HydrationBoundary>
+  );
+};
+
+export default TableListWrapper;
