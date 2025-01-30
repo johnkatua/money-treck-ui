6113f29301d3895824d4ac5ebbefe8af086ff1ae
johnkatua <johnkatua99@gmail.com>
2025-01-30 09:49:09 +0300

    add create expenditure form

--- app/(dashboard)/expenditure/CreateExpenditure.tsx
+++ app/(dashboard)/expenditure/CreateExpenditure.tsx
@@ -1,7 +1,44 @@
+"use client";
+
+import CustomButton from "@/app/components/buttons/CustomButton";
+import CustomFormField from "@/app/components/forms/CustomFormField";
+import CustomSelect from "@/app/components/forms/CustomSelect";
+import FormWrapper from "@/app/components/forms/FormWrapper";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const CreateExpenditure = () => {
-  return <div>Add Expenditure</div>;
+  const form = useForm<>({
+    resolver: zodResolver(),
+  });
+
+  const onSubmit = async (values) => {
+    console.log(values);
+  };
+
+  const { control } = form;
+  return (
+    <FormWrapper formTitle="Add New Expense" form={form} onSubmit={onSubmit}>
+      <CustomFormField control={control} name="name" label="Name *">
+        <Input placeholder="New Budget" />
+      </CustomFormField>
+      <CustomFormField control={control} name="amount" label="Amount *">
+        <Input type="number" />
+      </CustomFormField>
+      <CustomFormField control={control} name="budget_id" label="Budget *">
+        <CustomSelect placeholder="Select Budget">{[]}</CustomSelect>
+      </CustomFormField>
+      <div className="h-2" />
+      <CustomButton
+        text="Add Expense"
+        type="submit"
+        disabled={false}
+        className="w-full bg-green-500 hover:bg-green-600 text-white p-3 rounded-md transition-all duration-300"
+      />
+    </FormWrapper>
+  );
 };
 
 export default CreateExpenditure;
