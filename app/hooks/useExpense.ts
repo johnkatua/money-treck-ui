9bd1d226ac0316f097bcb1169982ebb084f50c07
johnkatua <johnkatua99@gmail.com>
2025-01-30 08:16:28 +0300

    add a use-expense query

---
+++ app/hooks/useExpense.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { EXPENSES_QUERY_KEY } from "../constants";
+import { getExpenses } from "../services/expenditure";
+
+export const useExpense = () => {
+  return useQuery({
+    queryKey: EXPENSES_QUERY_KEY,
+    queryFn: getExpenses,
+  });
+};
