e9da98a8d8002123b9c8c99cb20749545bda7ee8
johnkatua <johnkatua99@gmail.com>
2025-01-30 08:13:52 +0300

    add get-expenses service

---
+++ app/services/expenditure.ts
@@ -0,0 +1,22 @@
+import { axiosInterceptorInstance } from "../api/axios-interceptor-instance";
+import { IExpenditure, IExpenditureResponse } from "../types/Expenditure";
+
+export const getExpenses = async () => {
+  try {
+    const response = await axiosInterceptorInstance.get("/expenses");
+    const extractedData: IExpenditureResponse[] = response?.data?.data || [];
+
+    const updatedData: IExpenditure[] = extractedData.map((expense, idx) => ({
+      id: idx + 1,
+      ...expense,
+    }));
+
+    return updatedData;
+  } catch (error) {
+    console.error(
+      "Error occurred while fetching expenses:",
+      error instanceof Error ? error.message : error
+    );
+    throw new Error("Failed to fetch expenses. Please try again later.");
+  }
+};
