9eb2e9538c82f486885e8537b0f242080b9dfc9d
johnkatua <johnkatua99@gmail.com>
2025-01-30 10:28:07 +0300

    update revenue form schema

--- lib/definitions/RevenueFormSchema.ts
+++ lib/definitions/RevenueFormSchema.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
 
 export const RevenueFormSchema = z.object({
-  name: z.string(),
+  name: z.string().trim(),
+  amount: z
+    .number()
+    .min(10, {
+      message: "Be atleast 10 shillings",
+    })
+    .max(100, {
+      message: "Amount should not exceed ksh. 100",
+    }),
+  period: z.string().trim(),
 });
