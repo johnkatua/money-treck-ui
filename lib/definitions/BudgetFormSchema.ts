2d778f1ceaf83a7aa06799f491bb49cc8fe10375
johnkatua <johnkatua99@gmail.com>
2025-01-31 11:34:02 +0300

    add budget form schema

---
+++ lib/definitions/BudgetFormSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const BudgetFormSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "Name is required",
+    })
+    .trim(),
+  amount: z
+    .number()
+    .min(10, {
+      message: "Amount must be atleast Ksh. 10",
+    })
+    .max(100, {
+      message: "Amount should not exceed ksh. 100",
+    })
+    .or(z.string().transform((val) => Number(val))), // Handle string inputs and convert to number
+  period: z.enum(["daily", "weekly", "monthly", "yearly"], {
+    message: "Period must be either 'daily', 'weekly', 'monthly', or 'yearly'",
+  }),
+});
