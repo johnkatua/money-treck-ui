b71003109835b8144040cc5848b16436182491e0
johnkatua <johnkatua99@gmail.com>
2025-01-31 14:06:48 +0300

    add create-expenditure service

--- app/services/expenditure.ts
+++ app/services/expenditure.ts
@@ -1,5 +1,12 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
 import { axiosInterceptorInstance } from "../api/axios-interceptor-instance";
-import { IExpenditure, IExpenditureResponse } from "../types";
+import {
+  IExpenditure,
+  IExpenditureRequest,
+  IExpenditureResponse,
+} from "../types";
 
 export const getExpenses = async () => {
   try {
@@ -19,5 +26,24 @@ export const getExpenses = async () => {
       error instanceof Error ? error.message : error
     );
     throw new Error("Failed to fetch expenses. Please try again later.");
+  }
+};
+
+export const createExpense = async (values: IExpenditureRequest) => {
+  try {
+    const { name, amount, budget_id } = values;
+    await axiosInterceptorInstance.post("/expenses", {
+      name,
+      amount,
+      budget_id,
+    });
+  } catch (error) {
+    console.error(
+      "Error occurred while creating budget:",
+      error instanceof Error ? error.message : error
+    );
+    throw new Error("Failed to create budget. Please try again later.");
+  } finally {
+    revalidatePath("/expenditure");
   }
 };
--- app/types/Expenditure.ts
+++ app/types/Expenditure.ts
@@ -6,3 +6,9 @@ export interface IExpenditureResponse {
   name: string;
   amount: number;
 }
+
+export interface IExpenditureRequest {
+  name: string;
+  amount: number;
+  budget_id: string;
+}
