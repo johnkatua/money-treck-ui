6498a2a9e14c5b613d05ae4a60a06662b2534498
johnkatua <johnkatua99@gmail.com>
2025-01-31 14:23:48 +0300

    update expenditure form schema

---
+++ lib/definitions/ExpenditureFormSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const ExpenditureFormSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "Name is required",
+    })
+    .trim(),
+  amount: z
+    .number()
+    .min(10, {
+      message: "Amount must be atleast Ksh. 10",
+    })
+    .max(100, {
+      message: "Amount should not exceed ksh. 100",
+    })
+    .or(z.string().transform((val) => Number(val))), // Handle string inputs and convert to number
+  budget_id: z.string().min(1, {
+    message: "Budget is Required",
+  }),
+});
