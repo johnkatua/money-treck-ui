ce427df0b27741549c7eaac9cbc70877ea497270
johnkatua <johnkatua99@gmail.com>
2025-02-06 01:13:33 +0300

    update general settings page

--- app/(dashboard)/settings/GeneralSettings.tsx
+++ app/(dashboard)/settings/GeneralSettings.tsx
@@ -1,20 +1,11 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
+import Profile from "./Profile";
 
 const GeneralSettings = () => {
   return (
     <div className="grid grid-cols-1 gap-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>Profile</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex gap-4">
-            <div className="w-[25%] bg-slate-400">A</div>
-            <div className="w-[75%] bg-slate-400">B</div>
-          </div>
-        </CardContent>
-      </Card>
+      <Profile />
       <Card>
         <CardHeader>
           <CardTitle>Themes</CardTitle>
