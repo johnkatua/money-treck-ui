527b8d3a70e95c77acdb03be1555a5285bfc2b74
johnkatua <johnkatua99@gmail.com>
2025-02-06 21:29:48 +0300

    update method to update sheet state

--- app/stores/use-sheet.ts
+++ app/stores/use-sheet.ts
@@ -2,10 +2,10 @@ import { create } from "zustand";
 
 type SheetState = {
   open: boolean;
-  switchState: (open: boolean) => void;
+  switchSheetState: (open: boolean) => void;
 };
 
 export const useSheet = create<SheetState>((set) => ({
   open: false,
-  switchState: (open) => set({ open }),
+  switchSheetState: (open) => set({ open }),
 }));
