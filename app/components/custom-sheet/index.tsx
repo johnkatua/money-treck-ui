ee1e1e2e50de5ab6a48e01911419be6b5cf89f09
johnkatua <johnkatua99@gmail.com>
2025-02-06 14:06:16 +0300

    update custom sheet

--- app/components/custom-sheet/index.tsx
+++ app/components/custom-sheet/index.tsx
@@ -1,7 +1,37 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import React from "react";
 
 const CustomSheet = () => {
-  return <div></div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant={"outline"}>Open Sheet</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Sheet Title</SheetTitle>
+          <SheetDescription>
+            Make sure to close the sheet after you are done with it.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+      <div>
+        <p className="text-center">Sheet Content</p>
+      </div>
+      <SheetFooter>
+        <Button variant={"default"}>Close Sheet</Button>
+      </SheetFooter>
+    </Sheet>
+  );
 };
 
 export default CustomSheet;
