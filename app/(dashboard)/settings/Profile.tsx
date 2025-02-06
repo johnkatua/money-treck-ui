fa31c919087a167198716c06fd17fdee54246f8f
johnkatua <johnkatua99@gmail.com>
2025-02-06 12:19:34 +0300

    update profile page

--- app/(dashboard)/settings/Profile.tsx
+++ app/(dashboard)/settings/Profile.tsx
@@ -9,11 +9,10 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Pencil } from "lucide-react";
-import React from "react";
+import Image from "next/image";
 
 const Profile = () => {
-  const { data, isError, error, isLoading } = useUser();
-  console.log(data);
+  const { data, isError, error } = useUser();
 
   if (isError && data && data?.length < 0)
     return <div>Error: {error.message}</div>;
@@ -23,7 +22,7 @@ const Profile = () => {
       <CardHeader>
         <div className="flex items-center justify-between gap-4">
           <CardTitle className="w-[50%] text-4xl font-medium">
-            My Profile
+            {"Profile"}
           </CardTitle>
           <CardDescription>
             <span className="bg-gray-800 rounded-md w-8 h-8 text-white flex items-center justify-center cursor-pointer hover:bg-gray-950">
@@ -34,8 +33,35 @@ const Profile = () => {
       </CardHeader>
       <CardContent>
         <div className="flex gap-4">
-          <div className="w-[25%] bg-slate-400">A</div>
-          <div className="w-[75%] bg-slate-400">B</div>
+          <div className="w-[25%] h-[400px] shadow-md rounded-md hover:shadow-lg bg-slate-200">
+            <Image
+              src={data?.avatar || "/images/avatar.jpg"}
+              alt="profile"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-md"
+              priority
+            />
+          </div>
+          <div className="w-[75%] shadow-md rounded-md p-4 hover:shadow-lg">
+            <ul className="space-y-4">
+              <li>
+                <span className="font-semibold">Name:</span> {data?.name}
+              </li>
+              <li>
+                <span className="font-semibold">Email:</span> {data?.email}
+              </li>
+              <li>
+                <span className="font-semibold">Currency:</span>{" "}
+                {data?.currency || "KSH"}
+              </li>
+              <li>
+                <span className="font-semibold">Phone Number:</span>{" "}
+                {data?.phoneNumber || "N/A"}
+              </li>
+            </ul>
+          </div>
         </div>
       </CardContent>
     </Card>
