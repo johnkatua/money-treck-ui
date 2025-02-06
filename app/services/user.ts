be1c327a898da8f0c51e20035db2110bc7019a8a
johnkatua <johnkatua99@gmail.com>
2025-02-06 22:55:26 +0300

    add an update-user service

--- app/services/user.ts
+++ app/services/user.ts
@@ -1,4 +1,6 @@
+import { revalidatePath } from "next/cache";
 import { axiosInterceptorInstance } from "../api/axios-interceptor-instance";
+import { IUser } from "../types";
 
 export const getUser = async () => {
   try {
@@ -11,5 +13,23 @@ export const getUser = async () => {
       error instanceof Error ? error.message : error
     );
     throw new Error("Failed to fetch user. Please try again later.");
+  }
+};
+
+export const updateUser = async (values: IUser) => {
+  try {
+    const response = await axiosInterceptorInstance.put("/users", {
+      ...values,
+    });
+
+    return response.data.data.data;
+  } catch (error) {
+    console.error(
+      "Error occurred while updating user:",
+      error instanceof Error ? error.message : error
+    );
+    throw new Error("Failed to update user. Please try again later.");
+  } finally {
+    revalidatePath("/settings");
   }
 };
