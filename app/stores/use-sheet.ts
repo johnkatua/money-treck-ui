0b785782cb46335b1b11f8096b8157b74b02373f
johnkatua <johnkatua99@gmail.com>
2025-02-07 23:27:41 +0300

    refactor useSheet store to manage openSheetId instead of boolean state for improved state handling

--- app/stores/use-sheet.ts
+++ app/stores/use-sheet.ts
@@ -1,11 +1,11 @@
 import { create } from "zustand";
 
-type SheetState = {
-  open: boolean;
-  switchSheetState: (open: boolean) => void;
+type SheetStore = {
+  openSheetId: string | null;
+  switchSheetState: (id: string | null) => void;
 };
 
-export const useSheet = create<SheetState>((set) => ({
-  open: false,
-  switchSheetState: (open) => set({ open }),
+export const useSheet = create<SheetStore>((set) => ({
+  openSheetId: null,
+  switchSheetState: (id) => set({ openSheetId: id }),
 }));
