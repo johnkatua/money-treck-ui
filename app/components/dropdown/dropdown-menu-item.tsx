d4a9808e6ecfd6c60e280f959019f275d7e91623
johnkatua <johnkatua99@gmail.com>
2025-02-07 22:05:04 +0300

    add handleSelect prop to CustomDropdownMenuItem for improved interactivity

--- app/components/dropdown/dropdown-menu-item.tsx
+++ app/components/dropdown/dropdown-menu-item.tsx
@@ -5,17 +5,20 @@ interface CustomDropdownMenuItemProps {
   color: string;
   children: ReactNode;
   text: string;
+  handleSelect: () => void;
 }
 
 const CustomDropdownMenuItem: FC<CustomDropdownMenuItemProps> = ({
   color,
   children,
   text,
+  handleSelect,
 }) => {
   return (
     <DropdownMenuItem
       style={{ color }}
       className="flex items-center gap-2 px-4 py-2 cursor-pointer transition-all duration-300 rounded-md"
+      onClick={handleSelect}
     >
       {children}
       <span className="font-medium">{text}</span>
