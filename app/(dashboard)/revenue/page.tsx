594190c7f570d7edd588c253639de6e1d1c77256
johnkatua <johnkatua99@gmail.com>
2025-02-09 21:08:07 +0300

    feat: reorganize imports in Revenue page for better structure

--- app/(dashboard)/revenue/page.tsx
+++ app/(dashboard)/revenue/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { tabsHeader } from "@/lib/data";
-import { REVENEUS_QUERY_KEY } from "@/app/constants";
-import { getRevenues } from "@/app/services";
-import PageWrapper from "../../components/page-wrapper";
-import CreateRevenue from "./CreateRevenue";
+import CustomDialog from "@/app/components/custom-dialog";
 import TabWrapper from "@/app/components/tab-wrapper";
 import TableListWrapper from "@/app/components/table-wrapper/TableListWrapper";
+import { REVENEUS_QUERY_KEY } from "@/app/constants";
+import { getRevenues } from "@/app/services";
+import { tabsHeader } from "@/lib/data";
+import PageWrapper from "../../components/page-wrapper";
+import CreateRevenue from "./CreateRevenue";
 import RevenueList from "./RevenueList";
-import CustomDialog from "@/app/components/custom-dialog";
 
 const tabsContent = [
   {
