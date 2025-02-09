1b53080b76692bb84708ee1f477e8980c0c22f8d
johnkatua <johnkatua99@gmail.com>
2025-02-09 12:16:03 +0300

    feat: implement CustomDialog component with header and description for confirmation actions

--- app/components/custom-dialog/index.tsx
+++ app/components/custom-dialog/index.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { DialogHeader } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "@radix-ui/react-dialog";
+import { FC } from "react";
 
-const CustomDialog = () => {
-  return <div></div>;
+interface CustomDialogProps {
+  open: boolean;
+}
+
+const CustomDialog: FC<CustomDialogProps> = ({ open }) => {
+  return (
+    <Dialog open={open}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolute sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CustomDialog;
