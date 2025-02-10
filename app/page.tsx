eda3a92d6a1b6d8b37e02852fc836082c5048d0c
johnkatua <johnkatua99@gmail.com>
2025-02-10 13:35:03 +0300

    refactor: simplify Home component by removing unnecessary fragments

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import React from "react";
 import HomePage from "./(dashboard)";
-import CustomDialog from "./components/custom-dialog";
 
 const Home = () => {
-  return (
-    <>
-      <HomePage />
-    </>
-  );
+  return <HomePage />;
 };
 
 export default Home;
