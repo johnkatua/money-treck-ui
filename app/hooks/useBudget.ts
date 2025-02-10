10330490d79ea6f6526734ba01cf0ad7af513ff2
johnkatua <johnkatua99@gmail.com>
2025-02-10 23:52:47 +0300

    feat: add useUpdateBudget hook for budget updates

--- app/hooks/useBudget.ts
+++ app/hooks/useBudget.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { createBudget, getBudgets } from "../services";
+import { createBudget, getBudgets, updateBudget } from "../services";
 import { BUDGETS_QUERY_KEY } from "../constants";
 
 export const useBudget = () => {
@@ -20,5 +20,11 @@ export const useCreateBudget = () => {
         queryKey: BUDGETS_QUERY_KEY,
       });
     },
+  });
+};
+
+export const useUpdateBudget = () => {
+  return useMutation({
+    mutationFn: updateBudget,
   });
 };
