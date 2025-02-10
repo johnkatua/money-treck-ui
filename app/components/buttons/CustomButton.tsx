24a0bcecdd213a1b7f2fccc18ae3f02221fa9950
johnkatua <johnkatua99@gmail.com>
2025-02-10 13:48:26 +0300

    feat: add onClick prop to CustomButton for enhanced interactivity

--- app/components/buttons/CustomButton.tsx
+++ app/components/buttons/CustomButton.tsx
@@ -7,6 +7,7 @@ interface ICustomButton {
   type: "submit" | "reset";
   disabled: boolean;
   className?: string;
+  onClick?: () => void;
 }
 
 const CustomButton: FC<ICustomButton> = ({
@@ -14,9 +15,15 @@ const CustomButton: FC<ICustomButton> = ({
   type,
   disabled,
   className,
+  onClick,
 }) => {
   return (
-    <Button type={type} disabled={disabled} className={className}>
+    <Button
+      type={type}
+      disabled={disabled}
+      className={className}
+      onClick={onClick}
+    >
       {disabled && <Loader2 className="animate-spin" />}
       {text}
     </Button>
