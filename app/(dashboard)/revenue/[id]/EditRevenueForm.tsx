4103b707374ff1d1ea932dcd0b90275be39c7810
johnkatua <johnkatua99@gmail.com>
2025-02-10 17:17:20 +0300

    feat: enhance EditRevenueForm with dialog support for revenue deletion

--- app/(dashboard)/revenue/[id]/EditRevenueForm.tsx
+++ app/(dashboard)/revenue/[id]/EditRevenueForm.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import CustomButton from "@/app/components/buttons/CustomButton";
+import CustomDialog from "@/app/components/custom-dialog";
 import CustomFormField from "@/app/components/forms/CustomFormField";
 import CustomSelect from "@/app/components/forms/CustomSelect";
 import FormWrapper from "@/app/components/forms/FormWrapper";
 import { useUpdateRevenue } from "@/app/hooks";
-import { useRevenueStore } from "@/app/stores";
+import { useDialogStore, useRevenueStore } from "@/app/stores";
 import { Input } from "@/components/ui/input";
 import { SelectItem } from "@/components/ui/select";
 import { periodItems } from "@/lib/data";
@@ -19,6 +20,7 @@ import { z } from "zod";
 const EditRevenueForm = () => {
   const router = useRouter();
   const { selectedRevenue } = useRevenueStore();
+  const { openDialog } = useDialogStore();
   const { isPending, mutate } = useUpdateRevenue();
   const form = useForm<z.infer<typeof RevenueFormSchema>>({
     resolver: zodResolver(RevenueFormSchema),
@@ -57,46 +59,58 @@ const EditRevenueForm = () => {
 
   const { control } = form;
   return (
-    <FormWrapper
-      formTitle="Edit/Delete Revenue"
-      form={form}
-      onSubmit={onSubmit}
-      isUpdate={true}
-    >
-      <CustomFormField control={control} name="name" label="Revenue Name *">
-        <Input />
-      </CustomFormField>
-      <CustomFormField control={control} name="amount" label="Revenue Amount *">
-        <Input type="number" />
-      </CustomFormField>
-      <CustomFormField control={control} name="period" label="Revenue Period *">
-        <CustomSelect
-          placeholder="Select Period"
-          value={form.watch("period")}
-          onValueChange={(val) => form.setValue("period", val)}
+    <>
+      <FormWrapper
+        formTitle="Edit/Delete Revenue"
+        form={form}
+        onSubmit={onSubmit}
+        isUpdate={true}
+      >
+        <CustomFormField control={control} name="name" label="Revenue Name *">
+          <Input />
+        </CustomFormField>
+        <CustomFormField
+          control={control}
+          name="amount"
+          label="Revenue Amount *"
         >
-          {periodItems?.map(({ name, value }) => (
-            <SelectItem key={value} value={value}>
-              {name}
-            </SelectItem>
-          ))}
-        </CustomSelect>
-      </CustomFormField>
-      <div className="flex gap-4">
-        <CustomButton
-          text={isPending ? "Please Wait" : "Edit Revenue"}
-          type="submit"
-          disabled={isPending}
-          className="w-[200px] mt-2 bg-green-500 hover:bg-green-600 text-white p-3 rounded-md transition-all duration-300"
-        />
-        <CustomButton
-          text={"Delete Revenue"}
-          type="submit"
-          disabled={false}
-          className="w-[200px] mt-2 bg-red-500 hover:bg-red-600 text-white p-3 rounded-md transition-all duration-300"
-        />
-      </div>
-    </FormWrapper>
+          <Input type="number" />
+        </CustomFormField>
+        <CustomFormField
+          control={control}
+          name="period"
+          label="Revenue Period *"
+        >
+          <CustomSelect
+            placeholder="Select Period"
+            value={form.watch("period")}
+            onValueChange={(val) => form.setValue("period", val)}
+          >
+            {periodItems?.map(({ name, value }) => (
+              <SelectItem key={value} value={value}>
+                {name}
+              </SelectItem>
+            ))}
+          </CustomSelect>
+        </CustomFormField>
+        <div className="flex">
+          <CustomButton
+            text={isPending ? "Please Wait" : "Edit Revenue"}
+            type="submit"
+            disabled={isPending}
+            className="w-full mt-2 bg-green-500 hover:bg-green-600 text-white p-3 rounded-md transition-all duration-300"
+          />
+        </div>
+      </FormWrapper>
+      <CustomButton
+        text={"Delete Revenue"}
+        type="submit"
+        onClick={() => openDialog()}
+        disabled={false}
+        className="w-full mt-2 bg-black text-white p-3 rounded-md transition-all duration-300"
+      />
+      <CustomDialog />
+    </>
   );
 };
 
