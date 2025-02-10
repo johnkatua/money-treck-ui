8c4d450eaba1a0816cc153d0b46bac1eee39aff7
johnkatua <johnkatua99@gmail.com>
2025-02-10 14:55:12 +0300

    feat: implement EditRevenueForm with fields for name, amount, and period, including edit and delete buttons

--- app/(dashboard)/revenue/[id]/EditRevenueForm.tsx
+++ app/(dashboard)/revenue/[id]/EditRevenueForm.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import CustomButton from "@/app/components/buttons/CustomButton";
+import CustomFormField from "@/app/components/forms/CustomFormField";
+import CustomSelect from "@/app/components/forms/CustomSelect";
 import FormWrapper from "@/app/components/forms/FormWrapper";
+import { Input } from "@/components/ui/input";
+import { SelectItem } from "@/components/ui/select";
+import { periodItems } from "@/lib/data";
 import React from "react";
 import { useForm } from "react-hook-form";
 
 const EditRevenueForm = () => {
   const form = useForm({});
+
+  const { control } = form;
   return (
     <FormWrapper
       formTitle="Edit Revenue"
@@ -13,7 +21,38 @@ const EditRevenueForm = () => {
       onSubmit={() => {}}
       isUpdate={true}
     >
-      <div>Edit</div>
+      <CustomFormField control={control} name="name" label="Revenue Name *">
+        <Input />
+      </CustomFormField>
+      <CustomFormField control={control} name="amount" label="Revenue Amount *">
+        <Input type="number" />
+      </CustomFormField>
+      <CustomFormField control={control} name="period" label="Revenue Period *">
+        <CustomSelect
+          placeholder="Select Period"
+          onValueChange={(val) => form.setValue("period", val)}
+        >
+          {periodItems?.map(({ name, value }) => (
+            <SelectItem key={value} value={value}>
+              {name}
+            </SelectItem>
+          ))}
+        </CustomSelect>
+      </CustomFormField>
+      <div className="flex gap-4 mt-2">
+        <CustomButton
+          text={"Edit Revenue"}
+          type="submit"
+          disabled={false}
+          className="w-[250px] bg-green-500 hover:bg-green-600 text-white p-3 rounded-md transition-all duration-300"
+        />
+        <CustomButton
+          text={"Delete Revenue"}
+          type="submit"
+          disabled={false}
+          className="w-[250px] bg-red-500 hover:bg-red-600 text-white p-3 rounded-md transition-all duration-300"
+        />
+      </div>
     </FormWrapper>
   );
 };
