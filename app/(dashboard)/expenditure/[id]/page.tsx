4cc579d97c6c9078dccf7ed5d1f3e58739e22448
johnkatua <johnkatua99@gmail.com>
2025-02-10 23:59:39 +0300

    feat: add UpdateExpenditure component for expenditure updates

---
+++ app/(dashboard)/expenditure/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateExpenditure = () => {
+  return (
+    <div>
+      <div>Update Expenditure</div>
+    </div>
+  );
+};
+
+export default UpdateExpenditure;
