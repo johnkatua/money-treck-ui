4db1d3ee751001d42323d7161ba6f26924587b98
johnkatua <johnkatua99@gmail.com>
2025-02-10 16:44:59 +0300

    feat: update PageWrapper title to reflect Edit/Delete functionality for revenue

--- app/(dashboard)/revenue/[id]/page.tsx
+++ app/(dashboard)/revenue/[id]/page.tsx
@@ -4,7 +4,7 @@ import EditRevenueForm from "./EditRevenueForm";
 
 const EditRevenue = () => {
   return (
-    <PageWrapper title="Edit Revenue">
+    <PageWrapper title="Edit/Delete Revenue">
       <div className="px-4 py-4 bg-gradient-to-r from-pink-400 to-purple-600 rounded-lg shadow-2xl space-y-6">
         <EditRevenueForm />
       </div>
