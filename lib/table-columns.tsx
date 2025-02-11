d224a814f624895121eb8c2d90e379f5178c3176
johnkatua <johnkatua99@gmail.com>
2025-02-11 14:52:24 +0300

    feat: remove unused imports related to CustomDropDown in table-columns

--- lib/table-columns.tsx
+++ lib/table-columns.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import CustomActions from "@/app/components/actions";
-import CustomDropDown from "@/app/components/dropdown";
-import CustomDropdownMenuItem from "@/app/components/dropdown/dropdown-menu-item";
 import { useDialogStore, useRevenueStore, useSheet } from "@/app/stores";
 import { Revenue } from "@/app/types";
 import { IBudget } from "@/app/types/Budget";
 import { IExpenditure } from "@/app/types/Expenditure";
 import { Badge } from "@/components/ui/badge";
 import { ColumnDef } from "@tanstack/react-table";
-import { Edit, Trash } from "lucide-react";
 
 const periodType: Record<
   "daily" | "weekly" | "monthly" | "yearly",
