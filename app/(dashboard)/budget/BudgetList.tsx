4232f7595831a0fe9cb132791cb4a50b61206deb
johnkatua <johnkatua99@gmail.com>
2025-02-11 17:01:46 +0300

    feat: add EditBudget component to BudgetList and create expenditure store for selected expenditure management

--- app/(dashboard)/budget/BudgetList.tsx
+++ app/(dashboard)/budget/BudgetList.tsx
@@ -4,6 +4,7 @@ import TableWrapper from "@/app/components/table-wrapper";
 import { useBudget } from "@/app/hooks/useBudget";
 import { budgetColumns } from "@/lib/table-columns";
 import DeleteBudget from "./DeleteBudget";
+import EditBudget from "./EditBudget";
 
 const BudgetList = () => {
   const { data, isPending, isFetching, isLoading, isError, error } =
@@ -19,6 +20,7 @@ const BudgetList = () => {
         columns={budgetColumns}
         isLoading={isFetching || isPending || isLoading}
       />
+      <EditBudget />
       <DeleteBudget />
     </>
   );
---
+++ app/stores/use-expenditure.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+import { IExpenditureResponse } from "../types";
+
+type State = {
+  selectedExpenditure: IExpenditureResponse | null;
+};
+
+type Action = {
+  updateSelectedExpenditure: (
+    selectedExpenditureId: State["selectedExpenditure"]
+  ) => void;
+};
+
+export const useExpenditureStore = create<State & Action>((set) => ({
+  selectedExpenditure: null,
+  updateSelectedExpenditure: (selectedExpenditure) =>
+    set(() => ({
+      selectedExpenditure,
+    })),
+}));
