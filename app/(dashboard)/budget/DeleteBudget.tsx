2000a776fdbae1f7c91cc3ee9de47c4a3c6bc656
johnkatua <johnkatua99@gmail.com>
2025-02-11 15:39:57 +0300

    feat: implement budget deletion logic with error handling and user feedback

--- app/(dashboard)/budget/DeleteBudget.tsx
+++ app/(dashboard)/budget/DeleteBudget.tsx
@@ -1,10 +1,38 @@
 import CustomButton from "@/app/components/buttons/CustomButton";
 import CustomDialog from "@/app/components/custom-dialog";
 import { useDeleteBudget } from "@/app/hooks";
+import { useDialogStore } from "@/app/stores";
+import { useBudgetStore } from "@/app/stores/use-budget";
 import React from "react";
+import toast from "react-hot-toast";
 
 const DeleteBudget = () => {
+  const { selectedBudget } = useBudgetStore();
   const { isPending, mutate } = useDeleteBudget();
+  const { closeDialog } = useDialogStore();
+
+  const handleDelete = async () => {
+    const id = selectedBudget?._id;
+    if (!id) {
+      toast.error("Please select a budget");
+      closeDialog();
+      return;
+    }
+
+    mutate(id, {
+      onSuccess: () => {
+        toast.success("Budget deleted successfully");
+      },
+      onError: (error: unknown) => {
+        if (error instanceof Error) {
+          toast.error(error.message);
+        } else {
+          toast.error("An unexpected error occurred");
+        }
+      },
+      onSettled: () => closeDialog(),
+    });
+  };
   return (
     <CustomDialog
       description={`
@@ -15,7 +43,7 @@ const DeleteBudget = () => {
       <CustomButton
         text={isPending ? "Please Wait" : "Delete Budget"}
         type="submit"
-        onClick={() => {}}
+        onClick={handleDelete}
         disabled={isPending}
         className="w-full mt-2 bg-red-500 hover:bg-red-600 text-white p-3 rounded-md transition-all duration-300"
       />
