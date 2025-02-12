dd4e6c1f02385aa848d2c0c92576d7550d4e31e3
johnkatua <johnkatua99@gmail.com>
2025-02-12 16:45:54 +0300

    feat: integrate EditExpenditure component into ExpenditureList for enhanced editing functionality

--- app/(dashboard)/expenditure/ExpenditureList.tsx
+++ app/(dashboard)/expenditure/ExpenditureList.tsx
@@ -3,6 +3,7 @@
 import TableWrapper from "@/app/components/table-wrapper";
 import { useExpense } from "@/app/hooks/useExpense";
 import { expenseColumns } from "@/lib/table-columns";
+import EditExpenditure from "./EditExpenditure";
 
 const ExpenditureList = () => {
   const { data, isPending, isFetching, isLoading, isError, error } =
@@ -12,11 +13,14 @@ const ExpenditureList = () => {
     return <div>Error: {error.message}</div>;
 
   return (
-    <TableWrapper
-      data={data}
-      columns={expenseColumns}
-      isLoading={isFetching || isPending || isLoading}
-    />
+    <>
+      <TableWrapper
+        data={data}
+        columns={expenseColumns}
+        isLoading={isFetching || isPending || isLoading}
+      />
+      <EditExpenditure />
+    </>
   );
 };
 
