4a6022ea7ad647c2f130d9f44956d9390fc709d8
johnkatua <johnkatua99@gmail.com>
2025-02-12 16:42:11 +0300

    refactor: remove unused React import in EditExpenditure component

--- app/(dashboard)/expenditure/EditExpenditure.tsx
+++ app/(dashboard)/expenditure/EditExpenditure.tsx
@@ -1,6 +1,5 @@
 import CustomSheet from "@/app/components/custom-sheet";
 import { useExpenditureStore } from "@/app/stores/use-expenditure";
-import React from "react";
 import EditExpenditureForm from "./EditExpenditureForm";
 
 const EditExpenditure = () => {
