c2867da705ddc5425ff7dec0519a06dab7371a88
johnkatua <johnkatua99@gmail.com>
2025-02-20 13:25:11 +0300

    refactor: integrate pagination support in BudgetList component using usePaginationStore

--- app/(dashboard)/budget/BudgetList.tsx
+++ app/(dashboard)/budget/BudgetList.tsx
@@ -5,10 +5,14 @@ import { useBudget } from "@/app/hooks/useBudget";
 import { budgetColumns } from "@/lib/table-columns";
 import DeleteBudget from "./DeleteBudget";
 import EditBudget from "./EditBudget";
+import { usePaginationStore } from "@/app/stores/use-pagination";
 
 const BudgetList = () => {
-  const { data, isPending, isFetching, isLoading, isError, error } =
-    useBudget();
+  const { pageIndex, pageSize } = usePaginationStore();
+  const { data, isPending, isFetching, isLoading, isError, error } = useBudget(
+    pageIndex,
+    pageSize
+  );
 
   if (isError && data?.updatedData && data?.updatedData.length < 0)
     return <div>Error: {error.message}</div>;
@@ -19,6 +23,7 @@ const BudgetList = () => {
         data={data?.updatedData ?? []}
         columns={budgetColumns}
         isLoading={isFetching || isPending || isLoading}
+        rowCount={data?.total}
       />
       <EditBudget />
       <DeleteBudget />
