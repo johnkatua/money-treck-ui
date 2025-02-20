45b19df52b598cc5883d380e388197c79eaf98c1
johnkatua <johnkatua99@gmail.com>
2025-02-20 13:28:10 +0300

    refactor: reorder imports in page.tsx for better organization

--- app/(dashboard)/budget/page.tsx
+++ app/(dashboard)/budget/page.tsx
@@ -1,11 +1,11 @@
-import { tabsHeader } from "@/lib/data";
-import { BUDGETS_QUERY_KEY } from "@/app/constants";
-import { getBudgets } from "@/app/services/budget";
 import PageWrapper from "@/app/components/page-wrapper";
 import TabWrapper from "@/app/components/tab-wrapper";
-import CreateBudget from "./CreateBudget";
 import TableListWrapper from "@/app/components/table-wrapper/TableListWrapper";
+import { BUDGETS_QUERY_KEY } from "@/app/constants";
+import { getBudgets } from "@/app/services/budget";
+import { tabsHeader } from "@/lib/data";
 import BudgetList from "./BudgetList";
+import CreateBudget from "./CreateBudget";
 
 const tabsContent = [
   {
