7f217f8d17fbcf784322236a0cf48e024646eb04
johnkatua <johnkatua99@gmail.com>
2025-02-20 13:10:15 +0300

    refactor: remove unused console log and import of usePaginationStore in RevenueList component

--- app/(dashboard)/revenue/RevenueList.tsx
+++ app/(dashboard)/revenue/RevenueList.tsx
@@ -2,17 +2,15 @@
 
 import TableWrapper from "@/app/components/table-wrapper";
 import { useRevenues } from "@/app/hooks";
+import { usePaginationStore } from "@/app/stores/use-pagination";
 import { revenueColumns } from "@/lib/table-columns";
 import DeleteRevenue from "./DeleteRevenue";
 import EditRevenue from "./EditRevenue";
-import { usePaginationStore } from "@/app/stores/use-pagination";
 
 const RevenueList = () => {
   const { pageIndex, pageSize } = usePaginationStore();
   const { data, isPending, isFetching, isLoading, isError, error } =
     useRevenues(pageIndex, pageSize);
-
-  console.log({ pageIndex, pageSize });
 
   if (isError && data?.updatedData && data?.updatedData.length < 0)
     return <div>Error: {error.message}</div>;
