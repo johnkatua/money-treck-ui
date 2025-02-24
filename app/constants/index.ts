9feb0ed0130ebc56d35343edaf0eb9ea32d3c44f
johnkatua <johnkatua99@gmail.com>
2025-02-24 12:22:09 +0300

    feat: add BUDGET_UTILIZATION_QUERY_KEY constant for budget utilization queries

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,5 +1,6 @@
 export const REVENEUS_QUERY_KEY = "revenues";
 export const BUDGETS_QUERY_KEY = "budgets";
 export const FINANCIAL_OVERVIEW_QUERY_KEY = "financial-overview";
+export const BUDGET_UTILIZATION_QUERY_KEY = "budget-utilization";
 export const EXPENSES_QUERY_KEY = ["expenses"];
 export const USER_QUERY_KEY = ["user"];
