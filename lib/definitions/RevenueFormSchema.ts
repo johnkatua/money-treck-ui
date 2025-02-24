ca418f38a7660715be62484d496b3b72f828f7a4
johnkatua <johnkatua99@gmail.com>
2025-02-24 21:20:07 +0300

    refactor: remove max limit from RevenueFormSchema for amount validation

--- lib/definitions/RevenueFormSchema.ts
+++ lib/definitions/RevenueFormSchema.ts
@@ -13,9 +13,6 @@ export const RevenueFormSchema = z.object({
     .min(10, {
       message: "Amount must be atleast Ksh. 10",
     })
-    .max(10000, {
-      message: "Amount should not exceed ksh. 100",
-    })
     .or(z.string().transform((val) => Number(val))), // Handle string inputs and convert to number
   period: z.enum(["daily", "weekly", "monthly", "yearly"], {
     message: "Period must be either 'daily', 'weekly', 'monthly', or 'yearly'",
