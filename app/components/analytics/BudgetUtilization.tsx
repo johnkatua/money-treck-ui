0cb4fa748f2356ba350436054436550d5a4cb241
johnkatua <johnkatua99@gmail.com>
2025-02-24 16:27:39 +0300

    feat: add BudgetUtilization component to display budget utilization data

---
+++ app/components/analytics/BudgetUtilization.tsx
@@ -0,0 +1,10 @@
+import { useBudgetUtilization } from "@/app/hooks";
+import React from "react";
+
+const BudgetUtilization = () => {
+  const { data } = useBudgetUtilization();
+  console.log({ data });
+  return <div>BudgetUtilization</div>;
+};
+
+export default BudgetUtilization;
