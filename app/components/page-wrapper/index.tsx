058e09ce09978bc7dc3b95bc1da8281c4a00df85
johnkatua <johnkatua99@gmail.com>
2025-02-24 18:52:05 +0300

    feat: add menu button to PageWrapper component for improved navigation

--- app/components/page-wrapper/index.tsx
+++ app/components/page-wrapper/index.tsx
@@ -2,6 +2,7 @@
 
 import { FC, ReactNode } from "react";
 import Header from "../header";
+import { Menu } from "lucide-react";
 
 interface PageWrapperProps {
   title: string;
@@ -11,8 +12,11 @@ interface PageWrapperProps {
 const PageWrapper: FC<PageWrapperProps> = ({ title, children }) => {
   return (
     <div className="relative h-full w-full">
-      <div className="sticky top-0 left-0 p-2 bg-white shadow-md z-10">
+      <div className="flex items-center justify-between sticky top-0 left-0 p-2 bg-white shadow-md z-10">
         <Header title={title} />
+        <button className="text-white bg-[--theme-color-one] p-2 rounded-md">
+          <Menu size={24} />
+        </button>
       </div>
       <div className="p-4">{children}</div>
     </div>
