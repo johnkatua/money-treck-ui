9cbefb11a512f571384aeebb6a95cb6d4991470a
johnkatua <johnkatua99@gmail.com>
2025-02-24 12:07:58 +0300

    feat: rename IFinancialOverview to FinancialOverview and add BudgetUtilization interfaces

--- app/types/Report.ts
+++ app/types/Report.ts
@@ -1,5 +1,17 @@
-export interface IFinancialOverview {
+export interface FinancialOverview {
   totalRevenue: number;
   totalExpenses: number;
   netProfit: number;
+}
+
+export interface BudgetUtilizationData {
+  _id: string;
+  name: string;
+  totalBudget: number;
+  totalExpenses: number;
+  utilizationRate: number;
+}
+
+export interface BudgetUtilization {
+  data: BudgetUtilizationData[];
 }
