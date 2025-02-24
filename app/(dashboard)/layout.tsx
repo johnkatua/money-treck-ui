e8df675fd31bee447239175ce5f1a7ba41d9774f
johnkatua <johnkatua99@gmail.com>
2025-02-24 17:22:58 +0300

    feat: conditionally render sidebar in Dashboard component based on mobile view

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import { ReactNode } from "react";
 import CustomSidebar from "../components/sidebar/CustomSidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 const Dashboard = ({ children }: { children?: ReactNode }) => {
+  const isMobile = useIsMobile();
   return (
-    <div className="bg-[--theme-color-one] text-white h-screen flex gap-4">
-      <div className="w-[200px] p-2 overflow-hidden">
-        <CustomSidebar />
-      </div>
-      <div className="flex-1 py-4 pr-4">
+    <div className="bg-[--theme-color-one] text-white h-screen flex">
+      {!isMobile && (
+        <div className="w-[200px] p-2 overflow-hidden">
+          <CustomSidebar />
+        </div>
+      )}
+      <div className="flex-1 p-4">
         <div className="h-full bg-white text-[--theme-color-one] rounded-lg overflow-y-auto scrollbar">
           <div className="">{children}</div>
         </div>
