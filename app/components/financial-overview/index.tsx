b0df94b65e21bb653dcdc9eb6c2ee686cb4edefc
johnkatua <johnkatua99@gmail.com>
2025-02-24 13:00:10 +0300

    fix: handle potential null data in FinancialOverview component and add key to Card

--- app/components/financial-overview/index.tsx
+++ app/components/financial-overview/index.tsx
@@ -2,19 +2,16 @@
 
 import { useGetFinancialOverview } from "@/app/hooks";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import React from "react";
 
 const FinancialOverview = () => {
   const { data } = useGetFinancialOverview();
-  console.log({ data });
-  const objKeys = Object.keys(data);
-  console.log(objKeys);
+  const objKeys = Object.keys(data || {});
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-lg font-semibold">Financial Overview</h2>
       <section className="grid grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-4 mb-4">
         {objKeys?.map((key) => (
-          <Card>
+          <Card key={key}>
             <CardHeader>
               <CardTitle>{key}</CardTitle>
             </CardHeader>
