a21e25340039c15635ead273c721fe5636aef3ae
johnkatua <johnkatua99@gmail.com>
2025-02-24 16:52:28 +0300

    feat: add header to BudgetUtilization component for improved readability

--- app/components/analytics/BudgetUtilization.tsx
+++ app/components/analytics/BudgetUtilization.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import { useBudgetUtilization } from "@/app/hooks";
-import React from "react";
-import TableWrapper from "../table-wrapper";
 import { budgetUtilizationColumns } from "@/lib/table-columns";
+import TableWrapper from "../table-wrapper";
 
 const BudgetUtilization = () => {
   const { data, isLoading } = useBudgetUtilization();
-  console.log({ data });
   return (
-    <TableWrapper
-      data={data}
-      columns={budgetUtilizationColumns}
-      isLoading={isLoading}
-    />
+    <>
+      <h2 className="text-xl font-semibold tracking-wide mt-8">
+        Budget Utilization
+      </h2>
+      <TableWrapper
+        data={data}
+        columns={budgetUtilizationColumns}
+        isLoading={isLoading}
+      />
+    </>
   );
 };
 
